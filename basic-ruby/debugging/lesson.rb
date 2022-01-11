a4f2c4f86f42009f4ad864c32e7fd5128c04046a
plutos28 <gamevictor28@gmail.com>
2022-01-11 20:18:49 +0300

    create gemfile for pry-byebug

---
+++ basic-ruby/debugging/lesson.rb
@@ -0,0 +1,21 @@
+# using puts with nil, empty string or collection will 
+# print a blank line
+puts '--- Blank lines with puts ---'
+puts nil
+puts []
+puts ''
+
+# the inspect method provides string representations of 
+# objects that implement it, so you can use it to 
+# instead get string representations of the above cases
+puts '--- Inspect with puts ---'
+puts nil.inspect
+puts [].inspect
+puts ''.inspect
+
+# an alternative is to use p which is a combo of puts
+# and inspect like above
+puts '--- Using p ---'
+p nil
+p []
+p '' 
