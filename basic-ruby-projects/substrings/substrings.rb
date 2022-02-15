941e6b6cb552c842d157c370c1c5cc28e97bcbe5
plutos28 <gamevictor28@gmail.com>
2022-02-15 07:34:53 +0300

    return array with number of word repeated correctly

--- basic-ruby-projects/substrings/substrings.rb
+++ basic-ruby-projects/substrings/substrings.rb
@@ -1,15 +1,24 @@
 def substrings(string, dictionary)
-  words = {}
+  result = [] 
+  string = string.split
+
   dictionary.each do |word|
-    if string.include?(word)
-      words[word]++
+    string.each do |s|
+      if s.include?(word)
+        result << word 
+      end 
     end
   end
-  words
+
+  result
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+p substrings("below", dictionary) #=> ["below", "low"]
+p substrings("howdy how", dictionary) #=> ["how", "how", "howdy"]
+p substrings("sit it", dictionary) #=> ["it", "it", "i", "i", "sit"]
 
-p substrings("below", dictionary) # => { "below" => 1, "low" => 1 }
-# p substrings("Howdy partner, sit down! How's it going?", dictionary)
+#p substrings("below", dictionary) 
+# => { "below" => 1, "low" => 1 }
+#p substrings("Howdy partner, sit down! How's it going?", dictionary)
 # => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
